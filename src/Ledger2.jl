b0894e1da2ce8608c499c19b6022ab7cb05127a2
alecloudenback <alecloudenback@users.noreply.github.com>
2022-09-13 22:47:38 -0500

    initial

--- src/Ledger2.jl
+++ src/Ledger2.jl
@@ -1,5 +1,61 @@
 module Ledger2
 
-# Write your package code here.
+struct Ledger
+    asset
+    liability
+    expense
+    revenue
+    equity
+end
+
+function Ledger(;
+    asset=[:asset],
+    liability=[:liability],
+    expense=[:expense],
+    revenue=[:revenue],
+    equity=[:equity],
+    )
+
+
+
+    return Ledger(
+        map(a -> )
+
+end
+
+abstract type Account end
+abstract type CreditNormal <: Account end
+abstract type DebitNormal  <: Account end
+
+
+struct AssetAccount <: DebitNormal 
+    entries
+end
+struct LiabilityAccount <: CreditNormal 
+    entries
+end
+struct ExpenseAccount <: DebitNormal 
+    entries
+end
+struct RevenueAccount <: CreditNormal 
+    entries
+end
+struct EquityAccount <: CreditNormal 
+    entries
+end
+
+struct Entry{T,U}
+    credit::Symbol # credit account name
+    debit::Symbol  # debit account name
+    amount::T
+    time::U
+end
+
+
+
+
+assets(l::Ledger) = sum(l.assets)
+assets(l::Ledger,time) = sum(a in l.assets if a.time < time)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,20 @@ using Ledger2
 using Test
 
 @testset "Ledger2.jl" begin
-    # Write your tests here.
+
+    l = Ledger(...)
+
+    entries = [
+        # credit, debit, amt, time
+        Entry(:Cash,:Equity,100.,0), # equity raise 
+        Entry(:Cash,:Debt,50.,0),    # debt raise
+        Entry(:Equity,:Cash,10.,1),   # pay employees
+    ]
+
+    foreach(e -> post!(l,e),entries)
+
+    @test assets(l) == 140.
+    @test assets(l,0) == 150.
+
+
 end
