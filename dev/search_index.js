1e81fb78b3a3d7bbd8c106e5ca3780bc214b3e5e
Documenter.jl <documenter@juliadocs.github.io>
2022-09-14 03:36:05 +0000

    build based on 78a6c3f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Ledger2","category":"page"},{"location":"#Ledger2","page":"Home","title":"Ledger2","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Ledger2.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Ledger2]","category":"page"}]
+}
